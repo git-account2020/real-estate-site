5590ff02676017bffcfb11a1304b6f408cd42b4c
amin <ihelpmotivate@gmail.com>
2021-04-18 17:44:30 -0400

    properites: added h2 and p

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
-console.log('hello world!')
+function initMap(){
+  var location = {lat: -35.223789, lng: 80.841141};
+  var map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4, 
+    center: location
+  });
+}
